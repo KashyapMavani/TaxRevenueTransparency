844373f9be1c28545b615e19dc14617c80e8ce1f
FatGuyy <omkarjadhav2103@gmail.com>
2023-07-15 11:49:38 +0530

    Add code for organizations.

---
+++ contracts/centralGov.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CentralGovernment {
+    address public owner;
+    uint256 public totalFunds;
+    mapping(address => uint256) public balances;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function depositFunds() public payable {
+        balances[msg.sender] += msg.value;
+        totalFunds += msg.value;
+    }
+
+    function withdrawFunds(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance.");
+
+        balances[msg.sender] -= amount;
+        totalFunds -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getBalance(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function getTotalFunds() public view returns (uint256) {
+        return totalFunds;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
