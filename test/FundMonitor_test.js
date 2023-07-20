502062fc0c9a10eef16aaec8bfed7678115e9314
Atharva Waghchoure <95522743+AtharvaWaghchoure@users.noreply.github.com>
2023-07-20 13:59:17 +0530

    test added for allocate and transfer

--- test/FundMonitor_test.js
+++ test/FundMonitor_test.js
@@ -1,13 +1,63 @@
-const { assert } = require("console");
-const { contract } = require("web3/lib/commonjs/eth.exports")
+const FundMonitor = artifacts.require("FundMonitor");
 
-var FundMonitor = artifacts.require('FundMonitor')
+contract("FundMonitor", (accounts) => {
+  let fundMonitor;
 
-contract('FundMonitor', async function(){
-    // Tests the deployment
-    it('Tests the deployment of the FundMonitor', function(){
-        const fundMonitor = FundMonitor.deployed();
-        console.log("Address - ",fundMonitor.address);
-        assert(fundMonitor.address !== '');
-    })
+  // Deploy a new instance of the FundMonitor contract before each test
+  beforeEach(async () => {
+    fundMonitor = await FundMonitor.new();
+  });
+
+  // Test the allocation of funds
+  it("should allocate funds from one address to another", async () => {
+    const fromAddress = accounts[2];
+    const toAddress = accounts[3];
+    const amount = web3.utils.toWei("1", "ether");
+
+    await fundMonitor.addDistrict(fromAddress, "District1", {
+      from: accounts[0],
+    });
+    await fundMonitor.addSector(toAddress, "Sector1", { from: accounts[0] });
+
+    await fundMonitor.allocateFunds(fromAddress, toAddress, { value: amount });
+
+    const allocatedAmount = await fundMonitor.allocatedFunds(
+      fromAddress,
+      toAddress
+    );
+    assert.equal(
+      allocatedAmount.toString(),
+      amount,
+      "Incorrect allocated amount"
+    );
+  });
+
+  // Test the transfer of funds
+  it("should transfer funds from one address to another", async () => {
+    const fromAddress = accounts[2];
+    const toAddress = accounts[3];
+    const amount = web3.utils.toWei("1", "ether");
+
+    // Add districts and sectors
+    await fundMonitor.addDistrict(fromAddress, "District1", {
+      from: accounts[0],
+    });
+    await fundMonitor.addSector(toAddress, "Sector1", { from: accounts[0] });
+
+    // Allocate funds before transferring
+    await fundMonitor.allocateFunds(fromAddress, toAddress, { value: amount });
+
+    // Transfer funds
+    await fundMonitor.transferFunds(fromAddress, toAddress, amount);
+
+    const transferredAmount = await fundMonitor.transferredFunds(
+      fromAddress,
+      toAddress
+    );
+    assert.equal(
+      transferredAmount.toString(),
+      amount,
+      "Incorrect transferred amount"
+    );
+  });
 });
