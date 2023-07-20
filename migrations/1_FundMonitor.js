48fc8b296ef291f403be95a1406ea80bf6b48130
Atharva Waghchoure <95522743+AtharvaWaghchoure@users.noreply.github.com>
2023-07-20 13:59:57 +0530

    Update 1_FundMonitor.js

--- migrations/1_FundMonitor.js
+++ migrations/1_FundMonitor.js
@@ -1,8 +1,5 @@
 const FundMonitor = artifacts.require("FundMonitor");
 
-module.exports = function(deployer) {
-  var org1 = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
-  var org2 = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
-
-  deployer.deploy(FundMonitor, org1, org2);
+module.exports = function (deployer) {
+  deployer.deploy(FundMonitor);
 };
